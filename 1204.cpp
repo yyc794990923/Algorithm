a72922c503465c7e75f7ba16d228697270004bb9
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-06-12 22:47:03 +0800

    1204

---
+++ 1204.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 1204.cpp
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年06月12日 星期一 21时25分54秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin >> a >> b;
+    cout << a.find(b)+1 << endl;
+    return 0;
+}
